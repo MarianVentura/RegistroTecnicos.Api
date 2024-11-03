80dccfa073d06e20f4422b4abb445f82ca070404
MarianVentura <marianv1119@gmail.com>
2024-11-03 17:19:59 -0400

    Cree el contexto

---
+++ Context/TecnicosContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.Api.Models;
+using System.Collections.Generic;
+
+namespace RegistroTecnicos.Api.Context;
+
+public class TecnicosContext : DbContext
+{
+    public TecnicosContext(DbContextOptions<TecnicosContext> options) : base(options) { }
+    public DbSet<Clientes> Clientes { get; set; }
+}
