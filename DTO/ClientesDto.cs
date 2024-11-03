2759daa85a6bb2a43c1e98123508a277963b1577
MarianVentura <marianv1119@gmail.com>
2024-11-03 17:20:10 -0400

    Cree el Cliente DTO

---
+++ DTO/ClientesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RegistroTecnicos.Api.DTO;
+
+public class ClientesDto
+{
+    public int ClientesId { get; set; }
+    public string? Nombres { get; set; }
+    public string? WhatsApp {  get; set; }
+}
