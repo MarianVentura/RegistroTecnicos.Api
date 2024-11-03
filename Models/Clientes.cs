df135834393d35a7a0acd41fe533e8459d95720b
MarianVentura <marianv1119@gmail.com>
2024-11-03 17:20:29 -0400

    Cree el modelo de Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Api.Models;
+
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "El número de WhatsApp es obligatorio.")]
+    [Phone(ErrorMessage = "El número de WhatsApp no es válido.")]
+    public string? WhatsApp { get; set; }
+}
