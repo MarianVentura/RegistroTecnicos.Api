d8fd1dd3e6e0d8fb598079dcd0e47eca8d91fb86
MarianVentura <marianv1119@gmail.com>
2024-11-03 20:54:13 -0400

    Trabaje el ServicesRegistrar en la carpeta DI

--- Tecnicos.Services/DI/ServicesRegistrar.cs
+++ Tecnicos.Services/DI/ServicesRegistrar.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tecnicos.Abstractions;
+using Tecnicos.Domain.DI;
 
-namespace Tecnicos.Services.DI
+namespace Tecnicos.Services.DI;
+
+public static class ServicesRegistrar
 {
-    internal class ServicesRegistrar
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
+        services.RegisterDbContextFactory();
+        services.AddScoped<IClientesService, ClientesService>();
+        return services;
     }
 }
